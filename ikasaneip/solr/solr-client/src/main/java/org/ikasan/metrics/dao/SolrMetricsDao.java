41d9b78639ce4e3f1320ecb4af7dc61f5969cf7b
Michael Stewart <mick@humeenterprises.co.uk>
2021-06-13 18:43:18 +0100

    IKASAN-1876 moving rest libraries to dashboard and adding new metrics features

---
+++ ikasaneip/solr/solr-client/src/main/java/org/ikasan/metrics/dao/SolrMetricsDao.java
@@ -0,0 +1,168 @@
+package org.ikasan.metrics.dao;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrInputDocument;
+import org.ikasan.metrics.model.SolrFlowInvocationMetric;
+import org.ikasan.module.metadata.model.SolrModule;
+import org.ikasan.module.metadata.model.SolrModuleMetaDataImpl;
+import org.ikasan.spec.history.FlowInvocationMetric;
+import org.ikasan.spec.metadata.ModuleMetaData;
+import org.ikasan.spec.solr.SolrConstants;
+import org.ikasan.spec.solr.SolrDaoBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Ikasan Development Team.
+ */
+public class SolrMetricsDao extends SolrDaoBase<FlowInvocationMetric> {
+    /** Logger for this class */
+    private static Logger logger = LoggerFactory.getLogger(SolrMetricsDao.class);
+
+    /**
+     * We need to give this document it's context.
+     */
+    public static final String METRIC_ENTITY_TYPE = "metric";
+
+    private ObjectMapper mapper;
+
+    public SolrMetricsDao() {
+        this.mapper = new ObjectMapper();
+        this.mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Override
+    protected SolrInputDocument convertEntityToSolrInputDocument(Long expiry, FlowInvocationMetric flowInvocationMetric) {
+        SolrInputDocument document = new SolrInputDocument();
+        document.addField(ID, flowInvocationMetric.getModuleName() + "-" + METRIC_ENTITY_TYPE
+            + UUID.randomUUID());
+        document.addField(TYPE, METRIC_ENTITY_TYPE);
+        document.addField(MODULE_NAME, flowInvocationMetric.getModuleName());
+        document.addField(FLOW_NAME, flowInvocationMetric.getFlowName());
+
+        try {
+            document.addField(PAYLOAD_CONTENT, this.mapper.writeValueAsString(flowInvocationMetric));
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+            logger.warn(String.format("Could not set metric payload content[%s]", flowInvocationMetric), e);
+        }
+
+        document.addField(CREATED_DATE_TIME, flowInvocationMetric.getInvocationStartTime());
+        document.setField(EXPIRY, expiry);
+
+        return document;
+    }
+
+    public List<FlowInvocationMetric> getMetrics(long startTime, long endTime) {
+        try {
+            String query = super.buildQuery(Set.of(), Set.of(), Set.of(), new Date(startTime), new Date(endTime), null
+                , null, "metric", false);
+
+            List<FlowInvocationMetric> results = this.findByQuery(query)
+                .stream()
+                .map(bean -> convert(bean.getFlowInvocationMetric()))
+                .collect(Collectors.toList());
+
+            return results;
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Could not execute solr metrics query!", e);
+        }
+    }
+
+    public List<FlowInvocationMetric> getMetrics(String moduleName, long startTime, long endTime) {
+        try {
+            String query = super.buildQuery(Set.of(moduleName), Set.of(), Set.of(), new Date(startTime), new Date(endTime), null
+                , null, "metric", false);
+
+            List<FlowInvocationMetric> results = this.findByQuery(query)
+                .stream()
+                .map(bean -> convert(bean.getFlowInvocationMetric()))
+                .collect(Collectors.toList());
+
+            return results;
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Could not execute solr metrics query!", e);
+        }
+    }
+
+    public List<FlowInvocationMetric> getMetrics(String moduleName, String flowName, long startTime, long endTime) {
+        try {
+            String query = super.buildQuery(Set.of(moduleName), Set.of(flowName), Set.of(), new Date(startTime), new Date(endTime), null
+                , null, "metric", false);
+
+            List<FlowInvocationMetric> results = this.findByQuery(query)
+                .stream()
+                .map(bean -> convert(bean.getFlowInvocationMetric()))
+                .collect(Collectors.toList());
+
+            return results;
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Could not execute solr metrics query!", e);
+        }
+    }
+
+    /**
+     * Helper method to find by query.
+     *
+     * @param query
+     */
+    private List<SolrFlowInvocationMetric> findByQuery(String query)
+    {
+        logger.debug("queryString: " + query);
+
+        try
+        {
+            SolrQuery solrQuery = new SolrQuery();
+            solrQuery.setQuery(query);
+
+            QueryRequest req = new QueryRequest(solrQuery);
+            req.setBasicAuthCredentials(this.solrUsername, this.solrPassword);
+
+            QueryResponse rsp = req.process(this.solrClient, SolrConstants.CORE);
+
+            return rsp.getBeans(SolrFlowInvocationMetric.class);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error resolving solr flow invocation metric by query [" + query + "] " +
+                "from the ikasan solr index!", e);
+        }
+    }
+
+    /**
+     * Helper method to convert raw module metadata.
+     *
+     rawFlowInvocationMetric     * @return
+     */
+    private FlowInvocationMetric convert(String rawFlowInvocationMetric)
+    {
+        try
+        {
+            FlowInvocationMetric solrModuleMetaData
+                = mapper.readValue(rawFlowInvocationMetric, FlowInvocationMetric.class);
+
+            return solrModuleMetaData;
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(String.format("Unable to deserialise FlowInvocationMetric [%s]"
+                , rawFlowInvocationMetric), e);
+        }
+    }
+}
---
+++ ikasaneip/solr/solr-client/src/test/java/org/ikasan/metrics/dao/SolrMetricsDaoTest.java
@@ -0,0 +1,119 @@
+package org.ikasan.metrics.dao;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+import org.apache.solr.common.SolrInputDocument;
+import org.ikasan.metrics.model.ComponentInvocationMetricImpl;
+import org.ikasan.metrics.model.CustomMetric;
+import org.ikasan.metrics.model.FlowInvocationMetricImpl;
+import org.ikasan.metrics.model.MetricEventImpl;
+import org.ikasan.spec.solr.SolrDaoBase;
+import org.ikasan.wiretap.dao.SolrWiretapDao;
+import org.ikasan.wiretap.model.SolrWiretapEvent;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.json.JSONException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Ikasan Development Team on 04/08/2017.
+ */
+public class SolrMetricsDaoTest
+{
+    /**
+     * Mockery for mocking concrete classes
+     */
+    private Mockery mockery = new Mockery()
+    {
+        {
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }
+    };
+
+    private SolrClient server = mockery.mock(SolrClient.class);
+
+    @Test(expected = RuntimeException.class)
+    @DirtiesContext
+    public void test_save_exception() throws Exception
+    {
+        mockery.checking(new Expectations()
+        {
+            {
+                // set event factory
+                oneOf(server).request(with(any(UpdateRequest.class)));
+                will(throwException(new RuntimeException("Error")));
+
+            }
+        });
+
+        SolrMetricsDao dao = new SolrMetricsDao();
+        dao.setSolrClient(server);
+        dao.setDaysToKeep(0);
+
+        ComponentInvocationMetricImpl componentInvocationMetric = new ComponentInvocationMetricImpl();
+        componentInvocationMetric.setComponentName("my-component");
+        componentInvocationMetric.setStartTimeMillis(1000l);
+        componentInvocationMetric.setEndTimeMillis(2000l);
+        MetricEventImpl metricEvent = new MetricEventImpl();
+        metricEvent.setComponentName("my-component");
+        metricEvent.setFlowName("my-flow");
+        metricEvent.setEvent("event payload");
+        metricEvent.setTimestamp(1001l);
+        componentInvocationMetric.setWiretapFlowEvent(metricEvent);
+        CustomMetric customMetric = new CustomMetric();
+        customMetric.setName("name");
+        customMetric.setValue("value");
+        componentInvocationMetric.setMetrics(Set.of(customMetric));
+
+        FlowInvocationMetricImpl event = new FlowInvocationMetricImpl();
+        event.setModuleName("module-name");
+        event.setFlowName("my-flow");
+        event.setInvocationStartTime(1000l);
+        event.setInvocationEndTime(2000l);
+        event.setFlowInvocationEvents(Set.of(componentInvocationMetric));
+
+        dao.save(event);
+    }
+
+    @Test
+    public void test_convert_entity_to_solr_input_document() throws IOException, JSONException {
+        SolrMetricsDao dao = new SolrMetricsDao();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        List<FlowInvocationMetricImpl> event = objectMapper.readValue(loadDataFile("/data/flowInvocationMetrics.json")
+            , objectMapper.getTypeFactory().constructCollectionType(List.class, FlowInvocationMetricImpl.class));
+
+        SolrInputDocument solrInputDocument = dao.convertEntityToSolrInputDocument(1L, event.get(0));
+
+        Assert.assertEquals("My Module", solrInputDocument.getFieldValue(SolrDaoBase.MODULE_NAME));
+        Assert.assertEquals("metric", solrInputDocument.getFieldValue(SolrDaoBase.TYPE));
+        Assert.assertEquals("Trade Consumer Flow", solrInputDocument.getFieldValue(SolrDaoBase.FLOW_NAME));
+        JSONAssert.assertEquals("{\"id\":24271,\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"invocationStartTime\":1623325830486,\"invocationEndTime\":1623325830496,\"finalAction\":\"PUBLISH\",\"componentInvocationMetricImpls\":[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830491,\"endTimeMillis\":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id><asset><isin>XS2201078503</isin></asset><trade-details><nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7</amount><currency>USD</currency><trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book><party-details><counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491,\"eventId\":\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = null, producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out, transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = 1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, groupSequence = 0, targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util.ByteSequence@5e5e1ee4, marshalledProperties = org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, redeliveryCounter = 0, size = 0, properties = {IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer = false, text = <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486,\"eventId\":\"-1996324143\",\"relatedEventId\":null}}],\"harvested\":true,\"expiry\":1623930630497,\"errorUri\":null,\"harvestedDateTime\":1623325840029,\"flowInvocationEvents\":[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830491,\"endTimeMillis\":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id><asset><isin>XS2201078503</isin></asset><trade-details><nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7</amount><currency>USD</currency><trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book><party-details><counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491,\"eventId\":\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = null, producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out, transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = 1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, groupSequence = 0, targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util.ByteSequence@5e5e1ee4, marshalledProperties = org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, redeliveryCounter = 0, size = 0, properties = {IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer = false, text = <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486,\"eventId\":\"-1996324143\",\"relatedEventId\":null}}]}", (String)solrInputDocument.getFieldValue(SolrDaoBase.PAYLOAD_CONTENT), false);
+        Assert.assertEquals(1L, solrInputDocument.getFieldValue(SolrDaoBase.EXPIRY));
+    }
+
+    protected String loadDataFile(String fileName) throws IOException
+    {
+        String contentToSend = IOUtils.toString(loadDataFileStream(fileName), "UTF-8");
+
+        return contentToSend;
+    }
+
+    protected InputStream loadDataFileStream(String fileName) throws IOException
+    {
+        return getClass().getResourceAsStream(fileName);
+    }
+}
