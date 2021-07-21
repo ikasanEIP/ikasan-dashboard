33973f919a359374a5e629081afc0a27852f5c59
Michael Stewart <mick@humeenterprises.co.uk>
2021-07-21 20:23:28 +0200

    IKASAN-1874 fixing unit test.

--- ikasaneip/rest/rest-module-client/src/test/java/org/ikasan/rest/client/ModuleControlRestServiceImplTest.java
+++ ikasaneip/rest/rest-module-client/src/test/java/org/ikasan/rest/client/ModuleControlRestServiceImplTest.java
@@ -184,7 +184,7 @@ public class ModuleControlRestServiceImplTest
     @Test
     public void activationStatusForbidden()
     {
-        stubFor(put(urlEqualTo("/rest/moduleControl/isActivated/test%20Module%20Name"))
+        stubFor(get(urlEqualTo("/rest/moduleControl/isActivated/test%20Module%20Name"))
             .withHeader(HttpHeaders.CONTENT_TYPE, equalTo(MediaType.APPLICATION_JSON.toString()))
             .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON.toString()))
             .willReturn(aResponse()
