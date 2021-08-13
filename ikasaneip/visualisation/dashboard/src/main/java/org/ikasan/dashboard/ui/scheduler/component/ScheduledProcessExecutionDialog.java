2edd46001b4a7c7e364e49ec50a43bb11f76b175
Michael Stewart <mick@humeenterprises.co.uk>
2021-08-13 14:51:49 +0200

    IKASAN-1879 adding the translations for the process execution dialog

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/ScheduledProcessExecutionDialog.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/ScheduledProcessExecutionDialog.java
@@ -1,5 +1,6 @@
 package org.ikasan.dashboard.ui.scheduler.component;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.icon.Icon;
@@ -12,10 +13,8 @@ import com.vaadin.flow.data.converter.StringToIntegerConverter;
 import com.vaadin.flow.data.converter.StringToLongConverter;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterListener;
-import org.aspectj.weaver.loadtime.Agent;
 import org.ikasan.dashboard.ui.general.component.AbstractCloseableResizableDialog;
 import org.ikasan.dashboard.ui.util.DateFormatter;
-import org.ikasan.dashboard.ui.util.DateTimeUtil;
 import org.ikasan.spec.metadata.ModuleMetaData;
 import org.ikasan.spec.scheduled.ScheduledProcessEvent;
 
@@ -46,12 +45,12 @@ public class ScheduledProcessExecutionDialog extends AbstractCloseableResizableD
      */
     public ScheduledProcessExecutionDialog(ScheduledProcessEvent scheduledProcessEvent, ModuleMetaData agent) {
         super.showResize(false);
-        super.title.setText("Scheduled Job Execution Details");
+        super.title.setText(getTranslation("header.scheduled-job-execution-details", UI.getCurrent().getLocale()));
 
         formBinder = new Binder<>();
 
         FormLayout formLayout = new FormLayout();
-        this.agentNameTf = new TextField("Agent name");
+        this.agentNameTf = new TextField(getTranslation("label.agent", UI.getCurrent().getLocale()));
         this.agentNameTf.setEnabled(false);
         formBinder.forField(this.agentNameTf)
             .bind(ScheduledProcessEvent::getAgentName, ScheduledProcessEvent::setAgentName);
@@ -65,9 +64,9 @@ public class ScheduledProcessExecutionDialog extends AbstractCloseableResizableD
             Icon check = VaadinIcon.CHECK.create();
             check.getStyle().set("color", "#66bb6a");
             check.getStyle().set("font-size", "32pt");
-            check.getElement().setAttribute("title", "Job execution successful");
+            check.getElement().setAttribute("title", getTranslation("tooltip.job-execution-success", UI.getCurrent().getLocale()));
 
-            TextField status = new TextField("Execution status");
+            TextField status = new TextField(getTranslation("label.execution-status", UI.getCurrent().getLocale()));
             status.setValue("success");
             status.setEnabled(false);
             status.setSuffixComponent(check);
@@ -77,7 +76,7 @@ public class ScheduledProcessExecutionDialog extends AbstractCloseableResizableD
             Icon exclamation = VaadinIcon.EXCLAMATION.create();
             exclamation.getStyle().set("color", "#ef5350");
             exclamation.getStyle().set("font-size", "32pt");
-            exclamation.getElement().setAttribute("title", "Job execution failed");
+            exclamation.getElement().setAttribute("title", getTranslation("tooltip.job-execution-fail", UI.getCurrent().getLocale()));
 
             TextField status = new TextField("Execution Status");
             status.setValue("job failed");
@@ -86,58 +85,58 @@ public class ScheduledProcessExecutionDialog extends AbstractCloseableResizableD
             formLayout.add(status);
         }
 
-        this.agentUrlLf = new TextField("Agent URL");
+        this.agentUrlLf = new TextField(getTranslation("label.agent-url", UI.getCurrent().getLocale()));
         this.agentUrlLf.setPrefixComponent(link);
         this.agentUrlLf.setValue(" ");
         formLayout.add(this.agentUrlLf, 2);
 
-        this.jobNameTf = new TextField("Job name");
+        this.jobNameTf = new TextField(getTranslation("label.job-name", UI.getCurrent().getLocale()));
         this.jobNameTf.setEnabled(false);
         formBinder.forField(this.jobNameTf)
             .bind(ScheduledProcessEvent::getJobName, ScheduledProcessEvent::setJobName);
         formLayout.add(jobNameTf);
 
 
-        this.jobGroupTf = new TextField("Job group");
+        this.jobGroupTf = new TextField(getTranslation("label.job-group", UI.getCurrent().getLocale()));
         this.jobGroupTf.setEnabled(false);
         formBinder.forField(this.jobGroupTf)
             .bind(ScheduledProcessEvent::getJobGroup, ScheduledProcessEvent::setJobGroup);
         formLayout.add(jobGroupTf);
 
-        this.jobDescriptionTa = new TextArea("Job description");
+        this.jobDescriptionTa = new TextArea(getTranslation("label.job-description", UI.getCurrent().getLocale()));
         this.jobDescriptionTa.setEnabled(false);
         jobDescriptionTa.getStyle().set("minHeight", "100px");
         formBinder.forField(this.jobDescriptionTa)
             .bind(ScheduledProcessEvent::getJobDescription, ScheduledProcessEvent::setJobDescription);
         formLayout.add(jobDescriptionTa, 2);
 
-        this.commandLineTf = new TextArea("Command line");
+        this.commandLineTf = new TextArea(getTranslation("label.command-line", UI.getCurrent().getLocale()));
         this.commandLineTf.setEnabled(false);
         commandLineTf.getStyle().set("minHeight", "100px");
         formBinder.forField(this.commandLineTf)
             .bind(ScheduledProcessEvent::getCommandLine, ScheduledProcessEvent::setCommandLine);
         formLayout.add(commandLineTf, 2);
 
-        this.resultOutputTf = new TextField("Result output");
+        this.resultOutputTf = new TextField(getTranslation("label.result-output", UI.getCurrent().getLocale()));
         this.resultOutputTf.setEnabled(false);
         formBinder.forField(this.resultOutputTf)
             .bind(ScheduledProcessEvent::getResultOutput, ScheduledProcessEvent::setResultOutput);
         formLayout.add(resultOutputTf);
 
-        this.resultErrorTf = new TextField("Error output");
+        this.resultErrorTf = new TextField(getTranslation("label.error-output", UI.getCurrent().getLocale()));
         this.resultErrorTf.setEnabled(false);
         formBinder.forField(this.resultErrorTf)
             .bind(ScheduledProcessEvent::getResultError, ScheduledProcessEvent::setResultError);
         formLayout.add(resultErrorTf);
 
-        this.pidTf = new TextField("Process id");
+        this.pidTf = new TextField(getTranslation("label.process-id", UI.getCurrent().getLocale()));
         this.pidTf.setEnabled(false);
         formBinder.forField(this.pidTf)
-            .withConverter(new StringToLongConverter("Must be a number!"))
+            .withConverter(new StringToLongConverter(getTranslation("error.must-be-a-number", UI.getCurrent().getLocale())))
             .bind(ScheduledProcessEvent::getPid, ScheduledProcessEvent::setPid);
         formLayout.add(resultOutputTf);
 
-        this.userTf = new TextField("User");
+        this.userTf = new TextField(getTranslation("label.user", UI.getCurrent().getLocale()));
         this.userTf.setEnabled(false);
         formBinder.forField(this.userTf)
             .bind(ScheduledProcessEvent::getUser, ScheduledProcessEvent::setUser);
@@ -145,30 +144,31 @@ public class ScheduledProcessExecutionDialog extends AbstractCloseableResizableD
 
         DateFormatter dateFormatter = new DateFormatter();
 
-        this.fireTimeTf = new TextField("Job execution time");
+        this.fireTimeTf = new TextField(getTranslation("label.job-execution-time", UI.getCurrent().getLocale()));
         this.fireTimeTf.setEnabled(false);
         this.fireTimeTf.setValue(dateFormatter.getFormattedDate(scheduledProcessEvent.getFireTime()));
         formLayout.add(fireTimeTf);
 
-        this.nextFireTimeTf = new TextField("Next job execution time");
+        this.nextFireTimeTf = new TextField(getTranslation("label.next-job-execution-time", UI.getCurrent().getLocale()));
         this.nextFireTimeTf.setEnabled(false);
         this.nextFireTimeTf.setValue(dateFormatter.getFormattedDate(scheduledProcessEvent.getNextFireTime()));
         formLayout.add(nextFireTimeTf);
 
-        this.executionDurationTf = new TextField("Job duration");
+        this.executionDurationTf = new TextField(getTranslation("label.job-duration", UI.getCurrent().getLocale()));
         this.executionDurationTf.setEnabled(false);
         if(scheduledProcessEvent.isSuccessful()) {
-            this.executionDurationTf.setValue((scheduledProcessEvent.getCompletionTime() - scheduledProcessEvent.getFireTime()) + " milliseconds");
+            this.executionDurationTf.setValue((scheduledProcessEvent.getCompletionTime() - scheduledProcessEvent.getFireTime())
+                + " " + getTranslation("label.milliseconds", UI.getCurrent().getLocale()));
         }
         else{
-            this.executionDurationTf.setValue("Not applicable   ");
+            this.executionDurationTf.setValue(getTranslation("label.not-applicable", UI.getCurrent().getLocale()));
         }
         formLayout.add(executionDurationTf);
 
-        this.returnCodeTf = new TextField("Return code");
+        this.returnCodeTf = new TextField(getTranslation("label.return-code", UI.getCurrent().getLocale()));
         this.returnCodeTf.setEnabled(false);
         formBinder.forField(this.returnCodeTf)
-            .withConverter(new StringToIntegerConverter("Must be a number!"))
+            .withConverter(new StringToIntegerConverter(getTranslation("error.must-be-a-number", UI.getCurrent().getLocale())))
             .bind(ScheduledProcessEvent::getReturnCode, ScheduledProcessEvent::setReturnCode);
         formLayout.add(returnCodeTf);
 
