a98a4e07929e398297ac1cf3d917e7d6b0ff8460
Michael Stewart <mick@humeenterprises.co.uk>
2021-08-16 16:15:43 +0200

    IKASAN-1879 adding the translations for the dashboard modules widget

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/dashboard/component/ModuleWidget.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/dashboard/component/ModuleWidget.java
@@ -40,7 +40,7 @@ public class ModuleWidget extends Div {
 
         textField.setPrefixComponent(icon);
         HorizontalLayout layout = new HorizontalLayout();
-        H4 modules = new H4("Modules and Agents");
+        H4 modules = new H4(getTranslation("header.modules-and-agents", UI.getCurrent().getLocale()));
         layout.add(modules, textField);
         layout.setVerticalComponentAlignment(FlexComponent.Alignment.START, modules);
         layout.setVerticalComponentAlignment(FlexComponent.Alignment.END, textField);
@@ -77,7 +77,7 @@ public class ModuleWidget extends Div {
 
             String route = RouteConfiguration.forSessionScope()
                 .getUrl(GraphVisualisationDeepLinkView.class, VisualisationType.MODULE.name() + ":" + moduleMetaData.getName());
-            Anchor link = new Anchor(route, "view");
+            Anchor link = new Anchor(route, getTranslation("label.view", UI.getCurrent().getLocale()));
             link.setTarget("_blank");
             add(link);
             horizontalLayout.add(link);
