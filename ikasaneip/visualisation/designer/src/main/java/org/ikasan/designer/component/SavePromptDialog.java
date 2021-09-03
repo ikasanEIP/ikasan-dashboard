4c5a14727c55c1bc4baff9bffaa46105c96c3a67
Michael Stewart <mick@humeenterprises.co.uk>
2021-09-03 07:48:51 +0200

    IKASAN-1891 sorting out translation on unsaved diagram dialog on designer

--- ikasaneip/visualisation/designer/src/main/java/org/ikasan/designer/component/SavePromptDialog.java
+++ ikasaneip/visualisation/designer/src/main/java/org/ikasan/designer/component/SavePromptDialog.java
@@ -1,43 +1,18 @@
 package org.ikasan.designer.component;
 
-import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import org.ikasan.designer.action.DesignerAction;
 
-public class SavePromptDialog extends DesignerActionDialog {
-    private DesignerAction designerAction;
-
-    public SavePromptDialog(DesignerAction designerAction) {
-        this.designerAction = designerAction;
-        this.showResize(false);
-
-        H2 label = new H2("Your diagram has unsaved changes. Are you sure you want to continue?");
-
-        Button ok = new Button("OK");
-        Button cancel = new Button("Cancel");
-
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.add(ok, cancel);
-
-        ok.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> {
-            this.close();
-            if(designerAction != null) {
+public class SavePromptDialog extends ConfirmDialog {
+    public SavePromptDialog(DesignerAction designerAction, String header, String text,
+                            String okButton, String cancelButton) {
+        super(header, text, okButton
+            , (ComponentEventListener<ConfirmDialog.ConfirmEvent>) confirmEvent -> {
+                if(designerAction != null) {
                designerAction.execute();
             }
-        });
-
-        cancel.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> {
-            this.close();
-        });
-
-        this.content.add(label, buttonLayout);
-        this.content.setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER, label, buttonLayout);
-
-        this.setWidth("600px");
-        this.setHeight("250px");
+        }, cancelButton, (ComponentEventListener<ConfirmDialog.CancelEvent>) cancelEvent -> {});
+        super.setConfirmButtonTheme("error primary");
     }
 }
