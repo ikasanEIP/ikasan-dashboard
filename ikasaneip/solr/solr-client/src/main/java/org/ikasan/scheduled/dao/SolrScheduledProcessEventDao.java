5675364ac8e599e58fdec41510eeae323232177c
Michael Stewart <mick@humeenterprises.co.uk>
2021-11-05 22:21:51 +0100

    IKASAN-1995 sorting out search for recently completed scheduled jobs

--- ikasaneip/solr/solr-client/src/main/java/org/ikasan/scheduled/dao/SolrScheduledProcessEventDao.java
+++ ikasaneip/solr/solr-client/src/main/java/org/ikasan/scheduled/dao/SolrScheduledProcessEventDao.java
@@ -123,7 +123,7 @@ public class SolrScheduledProcessEventDao extends SolrDaoBase<ScheduledProcessEv
         }
 
         if(filter != null && !filter.isEmpty()) {
-            queryBuffer.append(" AND payload:\"*"+filter+"\"*");
+            queryBuffer.append(" AND (payload:*"+filter+"*").append(" OR payload:\""+filter).append("\")");
         }
 
         query.setQuery(queryBuffer.toString());
