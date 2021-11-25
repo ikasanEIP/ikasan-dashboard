ab6a4dd9e7c26b035a43dd986e4149350f63c58a
Michael Stewart <mick@humeenterprises.co.uk>
2021-11-25 23:53:57 +0100

    IKASAN-2017 adding functionality to format entities.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/general/component/ReplayDialog.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/general/component/ReplayDialog.java
@@ -200,7 +200,9 @@ public class ReplayDialog extends AbstractEntityViewDialog<IkasanSolrDocument>
             entityContentsViewDialog.populate(this.replayEvent);
         });
         HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.add(buttonWrapper, downloadButtonTooltip, newWindowButton);
+        buttonLayout.add(super.select, buttonWrapper, downloadButtonTooltip, newWindowButton);
+        buttonLayout.setVerticalComponentAlignment(FlexComponent.Alignment.START, super.select);
+        buttonLayout.setVerticalComponentAlignment(FlexComponent.Alignment.END, buttonWrapper, newWindowButton);
 
         VerticalLayout layout = new VerticalLayout();
         layout.add(headerLayout, formLayout, buttonLayout, replayButton);
