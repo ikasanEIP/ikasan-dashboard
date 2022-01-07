61704b3448d028f0955cf9e28b44771a5cabf969
Michael Stewart <mick@humeenterprises.co.uk>
2022-01-07 16:49:56 +0000

    IKASAN-1912 adding context template validator

---
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/scheduler/context/validation/InvalidContextTemplateException.java
@@ -0,0 +1,20 @@
+package org.ikasan.scheduler.context.validation;
+
+public class InvalidContextTemplateException extends Exception {
+
+    public InvalidContextTemplateException(String message) {
+        super(message);
+    }
+
+    public InvalidContextTemplateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidContextTemplateException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidContextTemplateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
