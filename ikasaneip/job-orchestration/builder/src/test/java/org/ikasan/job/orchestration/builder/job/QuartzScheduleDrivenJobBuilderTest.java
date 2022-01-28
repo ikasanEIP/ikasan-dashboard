8e9c68f3753833998a583b0b874ac41da3e1773b
Michael Stewart <mick@humeenterprises.co.uk>
2022-01-28 21:18:27 +0000

    IKASAN-1912 adding all endpoint config values to file and quartz scheduler jobs. Refactoring job orchestration rest.

--- ikasaneip/job-orchestration/builder/src/test/java/org/ikasan/job/orchestration/builder/job/QuartzScheduleDrivenJobBuilderTest.java
+++ ikasaneip/job-orchestration/builder/src/test/java/org/ikasan/job/orchestration/builder/job/QuartzScheduleDrivenJobBuilderTest.java
@@ -8,6 +8,8 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QuartzScheduleDrivenJobBuilderTest extends AbstractTest {
 
@@ -17,13 +19,23 @@ public class QuartzScheduleDrivenJobBuilderTest extends AbstractTest {
     public void test_builder_success() throws IOException, JSONException {
         QuartzScheduleDrivenJobBuilder quartzScheduleDrivenJobBuilder = new QuartzScheduleDrivenJobBuilder();
 
+        Map<String, String> passthroughProperties = new HashMap<>();
+        passthroughProperties.put("key", "value");
+
         quartzScheduleDrivenJobBuilder.withCronExpression("cronExpression")
             .withJobGroup("jobGroup")
             .withTimeZone("timezone")
+            .withEager(true)
+            .withIgnoreMisfire(true)
+            .withMaxEagerCallbacks(10)
+            .withPassthroughProperties(passthroughProperties)
+            .withPersistentRecovery(true)
+            .withRecoveryTolerance(100L)
             .withAgentName("agentName")
             .withContextId("contextId")
             .withDescription("description")
-            .withJobName("jobName");
+            .withJobName("jobName")
+            .withStartupControlType("MANUAL");
 
         JSONAssert.assertEquals(super.loadDataFile("/data/quartz-event-driven-job-builder-result.json"),
             service.getQuartzScheduleDrivenJobString(quartzScheduleDrivenJobBuilder.build()), JSONCompareMode.STRICT);
