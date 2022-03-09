685f3a94e41521918f0c6885e08f417152de904d
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-09 09:24:51 +0000

    IKASAN-1912 fixing broken build due to refactoring

--- ikasaneip/job-orchestration/model/src/main/java/org/ikasan/job/orchestration/model/job/QuartzScheduleDrivenJobImpl.java
+++ ikasaneip/job-orchestration/model/src/main/java/org/ikasan/job/orchestration/model/job/QuartzScheduleDrivenJobImpl.java
@@ -135,8 +135,13 @@ public class QuartzScheduleDrivenJobImpl extends SchedulerJobImpl implements Qua
         sb.append(", agentName='").append(agentName).append('\'');
         sb.append(", jobName='").append(jobName).append('\'');
         sb.append(", contextId='").append(contextId).append('\'');
-        sb.append(", childContextIds=[ ");
-        childContextIds.forEach(id -> sb.append("[").append(id).append("] "));
+        if(childContextIds != null) {
+            sb.append(", childContextIds=[ ");
+            childContextIds.forEach(id -> sb.append("[").append(id).append("] "));
+        }
+        else {
+            sb.append(", childContextIds='").append(this.childContextIds).append('\'');
+        }
         sb.append("], description='").append(description).append('\'');
         sb.append(", startupControlType='").append(startupControlType).append('\'');
         sb.append('}');
