6c91a492ff40d5be6d5b852ca0fa2056559829e6
John Otaegui <john@waddle.com.au>
2022-03-14 22:26:22 +1100

    IKASAN-2076 change query params to path varaibles for endpoint for status querying.

--- ikasaneip/job-orchestration/rest/rest-dashboard/src/main/java/org/ikasan/job/orchestration/rest/dashboard/status/ContextStatusServiceController.java
+++ ikasaneip/job-orchestration/rest/rest-dashboard/src/main/java/org/ikasan/job/orchestration/rest/dashboard/status/ContextStatusServiceController.java
@@ -41,8 +41,6 @@
 
 package org.ikasan.job.orchestration.rest.dashboard.status;
 
-import java.util.Map;
-
 import org.ikasan.job.orchestration.rest.dashboard.model.dto.ErrorDto;
 import org.ikasan.spec.scheduled.context.service.ContextStatusService;
 import org.slf4j.Logger;
@@ -50,9 +48,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/rest/context/status")
@@ -73,42 +71,19 @@ public class ContextStatusServiceController {
     }
 
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, path = {"/{instanceName}/{contextName}", "/{instanceName}/{contextName}/{jobIdentifier}"})
     @PreAuthorize("hasAnyAuthority('ALL','WebServiceAdmin')")
-    public ResponseEntity getContextStatus(@RequestParam Map<String, String> params) {
-        String instanceName = params.get("instanceName");
-        String contextName = params.get("contextName");
-
+    public ResponseEntity getContextStatusForJob(@PathVariable String instanceName,
+                                                 @PathVariable String contextName,
+                                                 @PathVariable(required = false) String jobIdentifier) {
         String contextNameStatus;
 
         try {
-            contextNameStatus = contextStatusService.getContextStatus(instanceName, contextName);
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-            String errorMessage =
-                String.format("An error has occurred attempting to get status for instance %s and context %s", instanceName, contextName);
-            return new ResponseEntity(
-                new ErrorDto(errorMessage + "! Error message ["
-                    + e.getMessage() + "]"), HttpStatus.BAD_REQUEST);
-        }
-
-        String infoMessage = String.format("Got status %s for instance %s and context %s", contextNameStatus, instanceName, contextName);
-        LOG.info(infoMessage);
-
-        return new ResponseEntity(contextNameStatus, HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/job")
-    @PreAuthorize("hasAnyAuthority('ALL','WebServiceAdmin')")
-    public ResponseEntity getContextStatusForJob(@RequestParam Map<String, String> params) {
-        String instanceName = params.get("instanceName");
-        String contextName = params.get("contextName");
-        String jobIdentifier = params.get("jobIdentifier");
-
-        String contextNameStatus;
-
-        try {
-            contextNameStatus = contextStatusService.getContextStatusForJob(instanceName, contextName, jobIdentifier);
+            if (jobIdentifier == null) {
+                contextNameStatus = contextStatusService.getContextStatus(instanceName, contextName);
+            } else {
+                contextNameStatus = contextStatusService.getContextStatusForJob(instanceName, contextName, jobIdentifier);
+            }
         } catch (Exception e) {
             LOG.error(e.getMessage());
             String errorMessage = String.format("An error has occurred attempting to get status for instance %s, context %s, jobIdentifier %s!",
@@ -124,5 +99,5 @@ public class ContextStatusServiceController {
 
         return new ResponseEntity(contextNameStatus, HttpStatus.OK);
     }
-    
+
 }
