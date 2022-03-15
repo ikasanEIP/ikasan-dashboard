8720ef130a5d7836e0376d5ba2beff0ab45c70ab
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-15 14:21:27 +0100

    IKASAN-1912 adding ability to ignore errors for when contexts are not in cache.

--- ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/integration/inbound/component/endpoint/ScheduleProcessInboundProducer.java
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/integration/inbound/component/endpoint/ScheduleProcessInboundProducer.java
@@ -3,15 +3,24 @@ package org.ikasan.job.orchestration.integration.inbound.component.endpoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ikasan.job.orchestration.context.cache.ContextMachineCache;
 import org.ikasan.job.orchestration.core.machine.ContextMachine;
+import org.ikasan.job.orchestration.integration.StartupApplicationListener;
+import org.ikasan.job.orchestration.integration.inbound.component.endpoint.configuration.ScheduleProcessInboundProducerConfiguration;
 import org.ikasan.job.orchestration.model.event.ContextualisedScheduledProcessEventImpl;
 import org.ikasan.job.orchestration.util.ObjectMapperFactory;
 import org.ikasan.spec.component.endpoint.EndpointException;
 import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.configuration.ConfiguredResource;
 import org.ikasan.spec.scheduled.event.model.ContextualisedScheduledProcessEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ScheduleProcessInboundProducer implements Producer<String> {
+public class ScheduleProcessInboundProducer implements Producer<String>, ConfiguredResource<ScheduleProcessInboundProducerConfiguration> {
+
+    private Logger logger = LoggerFactory.getLogger(ScheduleProcessInboundProducer.class);
 
     private ObjectMapper objectMapper = ObjectMapperFactory.newInstance();
+    private ScheduleProcessInboundProducerConfiguration configuration;
+    private String configurationId;
 
     @Override
     public void invoke(String payload) throws EndpointException {
@@ -20,10 +29,35 @@ public class ScheduleProcessInboundProducer implements Producer<String> {
                 = objectMapper.readValue(payload, ContextualisedScheduledProcessEventImpl.class);
             ContextMachine contextMachine = ContextMachineCache.instance()
                 .getByContextName(contextualisedScheduledProcessEvent.getContextId());
-                contextMachine.eventReceived(payload);
+            contextMachine.eventReceived(payload);
         }
         catch (Exception e) {
-            throw new EndpointException(e);
+            if(this.configuration.isIgnoreErrors()) {
+                logger.info("Ignoring error [{}] for payload [{}]", e.getMessage(), payload);
+            }
+            else {
+                throw new EndpointException(e);
+            }
         }
+    }
+
+    @Override
+    public String getConfiguredResourceId() {
+        return configurationId;
+    }
+
+    @Override
+    public void setConfiguredResourceId(String configurationId) {
+        this.configurationId = configurationId;
+    }
+
+    @Override
+    public ScheduleProcessInboundProducerConfiguration getConfiguration() {
+        return this.configuration;
+    }
+
+    @Override
+    public void setConfiguration(ScheduleProcessInboundProducerConfiguration scheduleProcessInboundProducerConfiguration) {
+        this.configuration = scheduleProcessInboundProducerConfiguration;
     }
 }
