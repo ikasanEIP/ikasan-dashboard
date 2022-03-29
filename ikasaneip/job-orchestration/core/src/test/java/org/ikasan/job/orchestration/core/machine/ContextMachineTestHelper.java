30a969360486f92f4e85a03dbe49ccb57d5289b5
John Otaegui <john@waddle.com.au>
2022-03-29 18:42:16 +1100

    IKASAN-2079 Finalise job lock cache and job logic machine get all tests passing.

---
+++ ikasaneip/job-orchestration/core/src/test/java/org/ikasan/job/orchestration/core/machine/ContextMachineTestHelper.java
@@ -0,0 +1,43 @@
+package org.ikasan.job.orchestration.core.machine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ikasan.job.orchestration.model.job.InternalEventDrivenJobImpl;
+import org.ikasan.spec.scheduled.context.model.ContextTemplate;
+import org.ikasan.spec.scheduled.job.model.InternalEventDrivenJob;
+
+public final class ContextMachineTestHelper {
+
+    public static Map<String, InternalEventDrivenJob> createInternalJobsMap(ContextTemplate contextTemplate) {
+        HashMap<String, InternalEventDrivenJob> internalEventDrivenJobs = new HashMap<>();
+
+        contextTemplate.getScheduledJobs().forEach(job -> internalEventDrivenJobs.put(job.getIdentifier(),
+            newInternalEventDrivenJob(job.getIdentifier())));
+
+        if (contextTemplate.getContexts() != null && !contextTemplate.getContexts().isEmpty()) {
+            contextTemplate.getContexts().forEach(template -> addInternalJobs(template, internalEventDrivenJobs));
+        }
+
+        return internalEventDrivenJobs;
+    }
+
+    private static void addInternalJobs(ContextTemplate contextTemplate, Map<String, InternalEventDrivenJob> internalEventDrivenJobs) {
+        if (contextTemplate.getContexts() == null || contextTemplate.getContexts().isEmpty()) {
+            contextTemplate.getScheduledJobs().forEach(job -> internalEventDrivenJobs.put(job.getIdentifier(),
+                newInternalEventDrivenJob(job.getIdentifier())));
+        } else {
+            contextTemplate.getContexts().forEach(template -> addInternalJobs(template, internalEventDrivenJobs));
+        }
+    }
+
+    private static InternalEventDrivenJob newInternalEventDrivenJob(String jobIdentifier) {
+        InternalEventDrivenJob job = new InternalEventDrivenJobImpl();
+        job.setIdentifier(jobIdentifier);
+        job.setChildContextIds(List.of("PASS_THROUGH"));
+
+        return job;
+    }
+
+}
