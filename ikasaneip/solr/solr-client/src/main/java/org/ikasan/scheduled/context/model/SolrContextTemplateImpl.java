8d9d06c0ddad151b0e0d73d11777bbc22085b72c
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-30 12:22:57 +0200

    IKASAN-2079 moving job locks to context namespace, and fixing issue with solr resolution of the context after lock were added to the data model.

--- ikasaneip/solr/solr-client/src/main/java/org/ikasan/scheduled/context/model/SolrContextTemplateImpl.java
+++ ikasaneip/solr/solr-client/src/main/java/org/ikasan/scheduled/context/model/SolrContextTemplateImpl.java
@@ -2,7 +2,7 @@ package org.ikasan.scheduled.context.model;
 
 import org.ikasan.spec.scheduled.context.model.ContextParameter;
 import org.ikasan.spec.scheduled.context.model.ContextTemplate;
-import org.ikasan.spec.scheduled.job.model.JobLock;
+import org.ikasan.spec.scheduled.context.model.JobLock;
 import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
 public class SolrContextTemplateImpl extends SolrContextImpl<ContextTemplate, ContextParameter, SchedulerJob, JobLock> implements ContextTemplate {
