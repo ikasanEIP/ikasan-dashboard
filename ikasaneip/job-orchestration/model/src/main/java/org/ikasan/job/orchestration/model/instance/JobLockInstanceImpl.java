2cf66c66656eb74310488dee6939bb23ac00bc1c
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-30 12:22:57 +0200

    IKASAN-2079 moving job locks to context namespace, and fixing issue with solr resolution of the context after lock were added to the data model.

--- ikasaneip/job-orchestration/model/src/main/java/org/ikasan/job/orchestration/model/instance/JobLockInstanceImpl.java
+++ ikasaneip/job-orchestration/model/src/main/java/org/ikasan/job/orchestration/model/instance/JobLockInstanceImpl.java
@@ -1,6 +1,6 @@
 package org.ikasan.job.orchestration.model.instance;
 
-import org.ikasan.job.orchestration.model.job.JobLockImpl;
+import org.ikasan.job.orchestration.model.context.JobLockImpl;
 import org.ikasan.spec.scheduled.instance.model.JobLockInstance;
 
 public class JobLockInstanceImpl extends JobLockImpl implements JobLockInstance {
