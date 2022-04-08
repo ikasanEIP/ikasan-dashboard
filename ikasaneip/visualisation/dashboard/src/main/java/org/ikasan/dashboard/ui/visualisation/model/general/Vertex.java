ac1ce03df8c42a1079d25036f7d12d8f774c5d85
Michael Stewart <mick@humeenterprises.co.uk>
2022-04-08 19:42:42 +0100

    IKASAN-1999 adding draw 2d model objects

---
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/model/general/Vertex.java
@@ -0,0 +1,22 @@
+package org.ikasan.dashboard.ui.visualisation.model.general;
+
+public class Vertex {
+    protected double x;
+    protected double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
