0d46fba3232394c7d91d0779857ce7241eb75baf
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-05-24 12:08:19 +0100

    IKASAN-2107 initial commit

---
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/core/notification/StopNotificationRunnerException.java
@@ -0,0 +1,11 @@
+package org.ikasan.job.orchestration.core.notification;
+
+public class StopNotificationRunnerException extends RuntimeException {
+
+    public StopNotificationRunnerException() {
+    }
+
+    public StopNotificationRunnerException(String message) {
+        super(message);
+    }
+}
