e2df9ccf18dc08576b21710fb257db4673975bf7
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-05-31 12:47:36 +0100

    IKASAN-2107 adding Monitor classes

---
+++ ikasaneip/job-orchestration/notification/src/test/java/org/ikasan/notification/monitor/mock/SchedulerJobRecordTestImpl.java
@@ -0,0 +1,66 @@
+package org.ikasan.notification.monitor.mock;
+
+import org.ikasan.scheduled.job.model.SolrFileEventDrivenJobImpl;
+import org.ikasan.spec.scheduled.job.model.FileEventDrivenJob;
+import org.ikasan.spec.scheduled.job.model.SchedulerJob;
+import org.ikasan.spec.scheduled.job.model.SchedulerJobRecord;
+
+public class SchedulerJobRecordTestImpl implements SchedulerJobRecord {
+    
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getAgentName() {
+        return null;
+    }
+
+    @Override
+    public String getJobName() {
+        return null;
+    }
+
+    @Override
+    public String getContextId() {
+        return null;
+    }
+
+    @Override
+    public SchedulerJob getJob() {
+        FileEventDrivenJob fileEventDrivenJob = new SolrFileEventDrivenJobImpl();
+        fileEventDrivenJob.setCronExpression("0 0/1 7-23 ? * MON-SUN *");
+        return fileEventDrivenJob;
+    }
+
+    @Override
+    public long getTimestamp() {
+        return 0;
+    }
+
+    @Override
+    public long getModifiedTimestamp() {
+        return 0;
+    }
+
+    @Override
+    public void setModifiedTimestamp(long timestamp) {
+
+    }
+
+    @Override
+    public String getModifiedBy() {
+        return null;
+    }
+
+    @Override
+    public void setModifiedBy(String modifiedBy) {
+
+    }
+}
