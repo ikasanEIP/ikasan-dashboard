49f5e38f771b6aa85d9bdc5c4cba32f704fbf2b2
Michael Stewart <mick@humeenterprises.co.uk>
2022-06-06 14:58:16 +0100

    IKASAN-2097 pushing static broadcasters behind the interface

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/service/ContextInstanceStateChangeEventBroadcasterImpl.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/service/ContextInstanceStateChangeEventBroadcasterImpl.java
@@ -1,8 +1,5 @@
 package org.ikasan.dashboard.ui.visualisation.scheduler.service;
 
-import java.util.LinkedList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 import org.ikasan.spec.scheduled.event.model.ContextInstanceStateChangeEvent;
@@ -12,29 +9,14 @@ import com.vaadin.flow.shared.Registration;
 
 public class ContextInstanceStateChangeEventBroadcasterImpl implements ContextInstanceStateChangeEventBroadcaster<Registration> {
 
-    private final Executor executor;
-    private final LinkedList<Consumer<ContextInstanceStateChangeEvent>> listeners;
-
-    public ContextInstanceStateChangeEventBroadcasterImpl() {
-        executor = Executors.newSingleThreadExecutor();
-        listeners = new LinkedList<>();
-    }
 
     @Override
     public synchronized Registration register(Consumer<ContextInstanceStateChangeEvent> listener) {
-        listeners.add(listener);
-
-        return () -> {
-            synchronized (ContextInstanceStateChangeEventBroadcasterImpl.class) {
-                listeners.remove(listener);
-            }
-        };
+        return org.ikasan.dashboard.ui.visualisation.scheduler.util.ContextInstanceStateChangeEventBroadcaster.register(listener);
     }
 
     @Override
     public synchronized void broadcast(ContextInstanceStateChangeEvent message) {
-        for (Consumer<ContextInstanceStateChangeEvent> listener : listeners) {
-            executor.execute(() -> listener.accept(message));
-        }
+        org.ikasan.dashboard.ui.visualisation.scheduler.util.ContextInstanceStateChangeEventBroadcaster.broadcast(message);
     }
 }
