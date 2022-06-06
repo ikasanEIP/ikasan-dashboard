0c4ec07f200e7c4a5d0afbc3506c91da1608fdc5
Michael Stewart <mick@humeenterprises.co.uk>
2022-06-06 14:58:16 +0100

    IKASAN-2097 pushing static broadcasters behind the interface

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/service/SchedulerJobStateChangeEventBroadcasterImpl.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/service/SchedulerJobStateChangeEventBroadcasterImpl.java
@@ -1,40 +1,20 @@
 package org.ikasan.dashboard.ui.visualisation.scheduler.service;
 
-import java.util.LinkedList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
-
+import com.vaadin.flow.shared.Registration;
 import org.ikasan.spec.scheduled.event.model.SchedulerJobInstanceStateChangeEvent;
 import org.ikasan.spec.scheduled.event.service.SchedulerJobStateChangeEventBroadcaster;
 
-import com.vaadin.flow.shared.Registration;
+import java.util.function.Consumer;
 
 public class SchedulerJobStateChangeEventBroadcasterImpl implements SchedulerJobStateChangeEventBroadcaster<Registration> {
 
-    private final Executor executor;
-    private final LinkedList<Consumer<SchedulerJobInstanceStateChangeEvent>> listeners;
-
-    public SchedulerJobStateChangeEventBroadcasterImpl() {
-        executor = Executors.newSingleThreadExecutor();
-        listeners = new LinkedList<>();
-    }
-
     @Override
     public synchronized Registration register(Consumer<SchedulerJobInstanceStateChangeEvent> listener) {
-        listeners.add(listener);
-
-        return () -> {
-            synchronized (SchedulerJobInstanceStateChangeEvent.class) {
-                listeners.remove(listener);
-            }
-        };
+        return org.ikasan.dashboard.ui.visualisation.scheduler.util.SchedulerJobStateChangeEventBroadcaster.register(listener);
     }
 
     @Override
     public synchronized void broadcast(SchedulerJobInstanceStateChangeEvent message) {
-        for (Consumer<SchedulerJobInstanceStateChangeEvent> listener : listeners) {
-            executor.execute(() -> listener.accept(message));
-        }
+        org.ikasan.dashboard.ui.visualisation.scheduler.util.SchedulerJobStateChangeEventBroadcaster.broadcast(message);
     }
 }
