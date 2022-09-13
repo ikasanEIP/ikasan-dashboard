13145d727d32cb72ef2febf0e6e52a8372f4a838
Michael Stewart <mick@humeenterprises.co.uk>
2022-09-13 16:17:18 +0100

    IKASAN-2127 breaking job and context visualisations into separate classes

---
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/component/ContextSchedulerVisualisation.java
@@ -0,0 +1,62 @@
+package org.ikasan.dashboard.ui.visualisation.scheduler.component;
+
+import org.ikasan.dashboard.ui.util.SystemEventLogger;
+import org.ikasan.designer.DesignerCanvas;
+import org.ikasan.scheduled.event.service.ScheduledProcessManagementService;
+import org.ikasan.security.service.SecurityService;
+import org.ikasan.security.service.UserService;
+import org.ikasan.spec.metadata.ModuleMetaDataService;
+import org.ikasan.spec.module.client.ConfigurationService;
+import org.ikasan.spec.module.client.LogStreamingService;
+import org.ikasan.spec.module.client.MetaDataService;
+import org.ikasan.spec.module.client.ModuleControlService;
+import org.ikasan.spec.scheduled.context.service.ScheduledContextService;
+import org.ikasan.spec.scheduled.instance.service.ScheduledContextInstanceService;
+import org.ikasan.spec.scheduled.instance.service.SchedulerJobInstanceService;
+import org.ikasan.spec.scheduled.job.model.SchedulerJob;
+import org.ikasan.spec.scheduled.job.model.SchedulerJobRecord;
+import org.ikasan.spec.scheduled.job.service.JobInitiationService;
+import org.ikasan.spec.scheduled.job.service.SchedulerJobService;
+import org.ikasan.spec.scheduled.profile.service.ContextProfileService;
+import org.ikasan.spec.scheduled.provision.JobProvisionService;
+import org.ikasan.spec.search.SearchResults;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ContextSchedulerVisualisation extends SchedulerVisualisation {
+
+    public ContextSchedulerVisualisation(String dynamicImagePath, ModuleMetaDataService moduleMetaDataService, ScheduledProcessManagementService scheduledProcessManagementService, ConfigurationService configurationRestService, ModuleControlService moduleControlRestService, MetaDataService metaDataRestService, SystemEventLogger systemEventLogger, SchedulerJobService schedulerJobService, LogStreamingService logStreamingService, SchedulerJobInstanceService schedulerJobInstanceService, JobInitiationService jobInitiationService, ContextProfileService contextProfileService, UserService userService, SecurityService securityService, ScheduledContextInstanceService scheduledContextInstanceService, JobProvisionService jobProvisionService, ScheduledContextService scheduledContextService) {
+        super(dynamicImagePath, moduleMetaDataService, scheduledProcessManagementService, configurationRestService, moduleControlRestService, metaDataRestService, systemEventLogger, schedulerJobService, logStreamingService, schedulerJobInstanceService, jobInitiationService, contextProfileService, userService, securityService, scheduledContextInstanceService, jobProvisionService, scheduledContextService);
+    }
+
+    protected void init() throws IOException {
+        if(!initialised && contextTemplate != null) {
+
+            if (this.designerCanvas != null) {
+                this.removeAll();
+            }
+
+            this.designerCanvas = new DesignerCanvas(this, null, "canvas-viewport-"+ UUID.randomUUID().toString(), this.dynamicImagePath, !this.edit);
+            this.designerCanvas.addCanvasInitialisedListener(this);
+
+            if(contextTemplate.getContexts() != null && !contextTemplate.getContexts().isEmpty()) {
+                this.designerCanvas.setCanvasJson(adapter.adaptContext(contextTemplate));
+            }
+
+            this.designerCanvas.addCanvasItemDoubleClickEventListener(this);
+            this.designerCanvas.addCanvasItemRightClickEventListener(this);
+            this.designerCanvas.addConnectorEventListener(this);
+            this.designerCanvas.addCanvasUpdatedListener(this);
+            this.designerCanvas.addFigureDeleteEventListeners(this);
+            this.designerCanvas.addFigureUndoDeleteEventListeners(this);
+
+            this.add(initCanvasActions(), designerCanvas);
+
+            this.initialised = true;
+        }
+    }
+}
