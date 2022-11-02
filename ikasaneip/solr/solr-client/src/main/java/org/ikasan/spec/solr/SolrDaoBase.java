7e26ff879baa98f23d23d11013905b98f3459f55
Michael Stewart <mick@humeenterprises.co.uk>
2022-11-02 07:23:40 +0000

    IKASAN-2151 finalising tree view and associated development.

--- ikasaneip/solr/solr-client/src/main/java/org/ikasan/spec/solr/SolrDaoBase.java
+++ ikasaneip/solr/solr-client/src/main/java/org/ikasan/spec/solr/SolrDaoBase.java
@@ -56,6 +56,7 @@ public abstract class SolrDaoBase<T> implements SolrInitialisationService
     public static final String HOSPITAL_EVENT_ACTION = "hospitalEventAction";
     public static final String STATUS = "status";
     public static final String MODIFIED_BY = "modifiedBy";
+    public static final String MANUALLY_SUBMITTED_BY = "manuallySubmittedBy";
     public static final String CHILD_CONTEXT_NAME = "childContextName";
     public static final String ACCESS_GROUPS = "accessGroups";
     public static final String ACCESS_USERS = "accessUsers";
