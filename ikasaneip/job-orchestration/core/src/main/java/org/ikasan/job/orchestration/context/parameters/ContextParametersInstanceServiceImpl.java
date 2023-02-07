4b609cc25ed98c8296eab614005f19db85daa5bd
Michael Stewart <mick@humeenterprises.co.uk>
2023-02-07 12:38:22 +0000

    IKASAN-2188 sorting out feature to use correlation identifier when resolving dynamic filenames

--- ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/context/parameters/ContextParametersInstanceServiceImpl.java
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/context/parameters/ContextParametersInstanceServiceImpl.java
@@ -1,7 +1,11 @@
 package org.ikasan.job.orchestration.context.parameters;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.ikasan.spec.scheduled.instance.model.ContextInstance;
 import org.ikasan.spec.scheduled.instance.model.ContextParameterInstance;
 import org.ikasan.spec.scheduled.instance.service.ContextParametersInstanceService;
 
@@ -36,4 +40,35 @@ public class ContextParametersInstanceServiceImpl implements ContextParametersIn
         return contextParametersFactory.getAllContextParameters(contextName);
     }
 
+    @Override
+    public void populateContextParametersOnContextInstance(ContextInstance contextInstance) {
+        List<ContextParameterInstance> propertyBackedContextParameterInstances
+            = this.getAllContextParameters(contextInstance.getName());
+
+        List<ContextParameterInstance> defaultContextParameterInstances
+            = contextInstance.getContextParameters();
+
+        /**
+         * Now create a map of all the property backed context parameters.
+         */
+        Map<String, ContextParameterInstance> finalContextParameters = new HashMap<>();
+        propertyBackedContextParameterInstances.forEach(contextParameterInstance
+            -> finalContextParameters.put(contextParameterInstance.getName(), contextParameterInstance));
+
+        /**
+         * Then iterate over the default parameter instances and add any to the final
+         * list that were not populated from properties.
+         */
+        defaultContextParameterInstances.forEach(contextParameterInstance -> {
+            if(!finalContextParameters.containsKey(contextParameterInstance.getName())) {
+                contextParameterInstance.setValue(contextParameterInstance.getDefaultValue());
+                finalContextParameters.put(contextParameterInstance.getName(), contextParameterInstance);
+            }
+        });
+
+        /**
+         * Now set the aggregated context parameters onto the context instance.
+         */
+        contextInstance.setContextParameters(finalContextParameters.values().stream().collect(Collectors.toList()));
+    }
 }
