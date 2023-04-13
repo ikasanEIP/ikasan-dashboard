59ac2cb7aeff23d0e8a5001622fa94cf744eb482
Michael Stewart <mick@humeenterprises.co.uk>
2023-04-13 06:00:16 +0100

    IKASAN-2223 removing update to job plan instance json when status chance events occur.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/util/NewSchedulerJobEventBroadcaster.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/util/NewSchedulerJobEventBroadcaster.java
@@ -12,24 +12,6 @@ import java.util.function.Consumer;
 public class NewSchedulerJobEventBroadcaster {
     static Executor executor = Executors.newSingleThreadExecutor();
 
-//    static LinkedList<Consumer<SchedulerJob>> listeners = new LinkedList<>();
-//
-//    public static synchronized Registration register(Consumer<SchedulerJob> listener) {
-//        listeners.add(listener);
-//
-//        return () -> {
-//            synchronized (NewSchedulerJobEventBroadcaster.class) {
-//                listeners.remove(listener);
-//            }
-//        };
-//    }
-//
-//    public static synchronized void broadcast(SchedulerJob schedulerJob) {
-//        for (Consumer<SchedulerJob> listener : listeners) {
-//            executor.execute(() -> listener.accept(schedulerJob));
-//        }
-//    }
-
     private static WeakHashMap<NewSchedulerJobEventBroadcastListener, Object> listeners =
         new WeakHashMap<>();
 
