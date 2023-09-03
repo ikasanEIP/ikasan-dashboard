dfb71baa63ad1b975ba7f83a59d32ac9ae773ca4
Michael Stewart <mick@humeenterprises.co.uk>
2023-09-03 08:35:02 +0100

    IKASAN-2282 sorting out status grid filtering

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/SchedulerStatusWidget.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/SchedulerStatusWidget.java
@@ -94,11 +94,8 @@ public class SchedulerStatusWidget extends Div implements FlowStateBroadcastList
         div.addClassNames("card-counter");
         div.setHeight("500px");
 
-        TextField textField = new TextField();
         Icon icon = VaadinIcon.SEARCH.create();
         icon.setSize("12pt");
-
-        textField.setPrefixComponent(icon);
 
         Div layout = new Div();
         layout.getElement().getStyle().set("margin-top", "20px");
@@ -107,11 +104,7 @@ public class SchedulerStatusWidget extends Div implements FlowStateBroadcastList
         Label flows = new Label(getTranslation("label.scheduler-agent-status", UI.getCurrent().getLocale()));
         flows.getElement().getStyle().set("font-size", "16pt");
 
-
-        textField.getElement().getStyle().set("margin-left", "auto");
-        textField.getElement().getStyle().set("float", "right");
-
-        layout.add(flows, textField);
+        layout.add(flows);
 
         this.runningDiv = new Div();
         this.runningDiv.addClassNames("card-counter", "running");
@@ -198,11 +191,12 @@ public class SchedulerStatusWidget extends Div implements FlowStateBroadcastList
         div.addClassNames("card-counter");
         div.setHeight("500px");
 
-        TextField textField = new TextField();
+        TextField filerTextField = new TextField();
+        filerTextField.setWidth("350px");
         Icon icon = VaadinIcon.SEARCH.create();
         icon.setSize("12pt");
 
-        textField.setPrefixComponent(icon);
+        filerTextField.setPrefixComponent(icon);
         Div layout = new Div();
         layout.getElement().getStyle().set("margin-top", "20px");
         layout.getElement().getStyle().set("margin-left", "10px");
@@ -218,10 +212,10 @@ public class SchedulerStatusWidget extends Div implements FlowStateBroadcastList
             this.createStatusView();
         });
 
-        textField.getElement().getStyle().set("margin-left", "auto");
-        textField.getElement().getStyle().set("float", "right");
+        filerTextField.getElement().getStyle().set("margin-left", "auto");
+        filerTextField.getElement().getStyle().set("float", "right");
 
-        layout.add(flows, returnIcon, textField);
+        layout.add(flows, returnIcon, filerTextField);
 
 
         // Create a modulesGrid bound to the list
@@ -250,6 +244,7 @@ public class SchedulerStatusWidget extends Div implements FlowStateBroadcastList
             return horizontalLayout;
         }));
         div.add(layout, this.flowsGrid);
+        this.flowsGrid.addGridFiltering(filerTextField, this.flowSearchFilter::setFlowNameFilter);
 
         this.flowsGrid.init();
 
