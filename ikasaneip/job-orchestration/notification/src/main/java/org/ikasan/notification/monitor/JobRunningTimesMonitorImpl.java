024fe7ddb8b5de71e2297f09ec4a9c407138d139
Tan Richard <Richard.Tan@uk.mizuho-sc.com>
2023-10-04 18:45:09 +0100

    KASAN-2298 - small fix to remove unnecessary noise in logs when there is nothing wrong

--- ikasaneip/job-orchestration/notification/src/main/java/org/ikasan/notification/monitor/JobRunningTimesMonitorImpl.java
+++ ikasaneip/job-orchestration/notification/src/main/java/org/ikasan/notification/monitor/JobRunningTimesMonitorImpl.java
@@ -107,8 +107,9 @@ public class JobRunningTimesMonitorImpl extends AbstractMonitorBase<GenericNotif
                 if (contextInstance.getStatus().toString().equalsIgnoreCase(InstanceStatus.COMPLETE.toString()) ||
                     contextInstance.getStatus().toString().equalsIgnoreCase(InstanceStatus.ENDED.toString())) {
                     unregister(contextInstance);
-                    throw new StopNotificationRunnerException("Context:" + contextInstance.getName() + ", InstanceId: " + contextInstance.getId() +
+                    LOG.info("Context:" + contextInstance.getName() + ", InstanceId: " + contextInstance.getId() +
                         " is " + contextInstance.getStatus().toString() + ", stopping the JobRunningTimesNotificationsRunner");
+                    return;
                 }
 
                 if (contextInstance.getStatus().toString().equalsIgnoreCase(InstanceStatus.RUNNING.toString()) ||
