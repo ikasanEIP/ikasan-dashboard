8060341f93e5428a84db4efdc3898702d47fcbb9
Tan Richard <Richard.Tan@uk.mizuho-sc.com>
2023-10-06 11:10:17 +0100

    KASAN-2298 - small fix to import

--- ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/context/cache/ContextMachineCache.java
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/context/cache/ContextMachineCache.java
@@ -2,13 +2,12 @@ package org.ikasan.job.orchestration.context.cache;
 
 import org.apache.commons.lang3.StringUtils;
 import org.ikasan.job.orchestration.core.machine.ContextMachine;
+import org.ikasan.spec.scheduled.instance.model.InstanceStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
