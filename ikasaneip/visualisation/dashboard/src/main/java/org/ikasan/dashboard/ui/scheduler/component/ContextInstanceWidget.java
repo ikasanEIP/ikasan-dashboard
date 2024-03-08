e358df11189059a2de5a39cb433e197e3955fe07
Michael Stewart <mick@humeenterprises.co.uk>
2024-03-08 03:25:22 +0000

    IKASAN-2349 cleaning up the pom files.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/ContextInstanceWidget.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/ContextInstanceWidget.java
@@ -1,9 +1,6 @@
 package org.ikasan.dashboard.ui.scheduler.component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.hilerio.ace.AceEditor;
-import com.hilerio.ace.AceMode;
-import com.hilerio.ace.AceTheme;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.UI;
@@ -27,9 +24,9 @@ import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.RouteConfiguration;
-//import de.f0rce.ace.AceEditor;
-//import de.f0rce.ace.enums.AceMode;
-//import de.f0rce.ace.enums.AceTheme;
+import de.f0rce.ace.AceEditor;
+import de.f0rce.ace.enums.AceMode;
+import de.f0rce.ace.enums.AceTheme;
 import org.ikasan.dashboard.ui.general.component.NotificationHelper;
 import org.ikasan.dashboard.ui.scheduler.command.HoldAllCommandExecutionJobsForContextInstanceCommand;
 import org.ikasan.dashboard.ui.scheduler.command.ReleaseAllCommandExecutionJobsForContextInstanceCommand;
