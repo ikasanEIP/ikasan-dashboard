9b9020aa3fc68ed682a9b4d6f97512d091d2c939
Michael Stewart <mick@humeenterprises.co.uk>
2024-08-17 11:31:12 +0100

    IKASAN-2450 set collections on context implementations to empty collections.

--- ikasaneip/job-orchestration/model/src/test/java/org/ikasan/job/orchestration/util/ContextHelperTest.java
+++ ikasaneip/job-orchestration/model/src/test/java/org/ikasan/job/orchestration/util/ContextHelperTest.java
@@ -67,6 +67,19 @@ public class ContextHelperTest {
     }
 
     @Test
+    public void test_context_template_token_replacement_with_no_job_dependencies() throws IOException, JSONException {
+        ContextTemplate contextTemplate = this.contextService
+            .getContextTemplate(loadDataFile("/data/job-plan-no-dependencies.json"));
+
+        contextHelper.setUseUnderscoreSeparatedContextNameConvention(true);
+        ContextHelper.addContextTemplateReplacementTokens(contextTemplate);
+
+        Assert.assertNotNull(contextTemplate);
+        JSONAssert.assertEquals(loadDataFile("/data/job-plan-no-dependencies-with-tokens.json")
+            , ObjectMapperFactory.newInstance().writeValueAsString(contextTemplate), JSONCompareMode.LENIENT);
+    }
+
+    @Test
     public void test_context_template_token_replacement_not_using_underscore_convention() throws IOException, JSONException {
         ContextTemplate contextTemplate = this.contextService
             .getContextTemplate(loadDataFile("/data/-1793100514.json"));
