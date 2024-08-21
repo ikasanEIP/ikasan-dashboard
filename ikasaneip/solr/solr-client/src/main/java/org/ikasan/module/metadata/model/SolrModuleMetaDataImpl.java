0c77ac4f6f298e903eb657ad877a3fc5c9a19c7f
Michael Stewart <mick@humeenterprises.co.uk>
2024-08-21 07:16:47 +0100

    IKASAN-2451 adding ikasan version to relevant pojos

--- ikasaneip/solr/solr-client/src/main/java/org/ikasan/module/metadata/model/SolrModuleMetaDataImpl.java
+++ ikasaneip/solr/solr-client/src/main/java/org/ikasan/module/metadata/model/SolrModuleMetaDataImpl.java
@@ -19,6 +19,7 @@ public class SolrModuleMetaDataImpl implements ModuleMetaData
     private String name;
     private String description;
     private String version;
+    private String ikasanVersion;
     private List<FlowMetaData> flows;
     private String configuredResourceId;
 
@@ -66,6 +67,16 @@ public class SolrModuleMetaDataImpl implements ModuleMetaData
     public String getVersion()
     {
         return this.version;
+    }
+
+    @Override
+    public String getIkasanVersion() {
+        return ikasanVersion;
+    }
+
+    @Override
+    public void setIkasanVersion(String ikasanVersion) {
+        this.ikasanVersion = ikasanVersion;
     }
 
     @Override
