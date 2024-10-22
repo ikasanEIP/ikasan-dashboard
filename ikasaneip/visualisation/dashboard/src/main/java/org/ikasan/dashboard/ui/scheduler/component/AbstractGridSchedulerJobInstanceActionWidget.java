3d0f094b1fb6b66a96873dc7cd70e4e0bb975446
Michael Stewart <mick@humeenterprises.co.uk>
2024-10-22 10:37:54 +0100

    IKASAN-2495 determine jobs that a job resides within from the job plan rather than the values set on the job itself.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/AbstractGridSchedulerJobInstanceActionWidget.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/scheduler/component/AbstractGridSchedulerJobInstanceActionWidget.java
@@ -316,7 +316,9 @@ public abstract class AbstractGridSchedulerJobInstanceActionWidget extends Div {
                         contextMachine.resetJob(schedulerJobInstance.getIdentifier(), schedulerJobInstance.getChildContextName());
                     }
                     else {
-                        schedulerJobInstance.getChildContextNames().forEach(name
+                        ContextHelper.getContextsWhereJobFilterMatchResides
+                            (this.contextInstance, schedulerJobInstance.getJobName())
+                            .forEach(name
                             -> contextMachine.resetJob(schedulerJobInstance.getIdentifier(), name));
                     }
                 }
