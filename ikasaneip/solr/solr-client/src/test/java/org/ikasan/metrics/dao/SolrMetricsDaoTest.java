00b6def9bf1d0c7d08773fdd1559f009672428cd
Michael Stewart <mick@humeenterprises.co.uk>
2025-07-15 04:20:59 +0100

    IKASAN-2599 add harvesting time to harvest metrics flow invocation records

--- ikasaneip/solr/solr-client/src/test/java/org/ikasan/metrics/dao/SolrMetricsDaoTest.java
+++ ikasaneip/solr/solr-client/src/test/java/org/ikasan/metrics/dao/SolrMetricsDaoTest.java
@@ -25,7 +25,10 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.Customization;
 import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.util.FileSystemUtils;
 
@@ -143,7 +146,54 @@ public class SolrMetricsDaoTest extends SolrTestCaseJ4
         Assert.assertEquals("My Module", solrInputDocument.getFieldValue(SolrDaoBase.MODULE_NAME));
         Assert.assertEquals("metric", solrInputDocument.getFieldValue(SolrDaoBase.TYPE));
         Assert.assertEquals("Trade Consumer Flow", solrInputDocument.getFieldValue(SolrDaoBase.FLOW_NAME));
-        JSONAssert.assertEquals("{\"id\":24271,\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"invocationStartTime\":1623325830486,\"invocationEndTime\":1623325830496,\"finalAction\":\"PUBLISH\",\"componentInvocationMetricImpls\":[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830491,\"endTimeMillis\":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id><asset><isin>XS2201078503</isin></asset><trade-details><nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7</amount><currency>USD</currency><trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book><party-details><counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491,\"eventId\":\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = null, producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out, transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = 1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, groupSequence = 0, targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util.ByteSequence@5e5e1ee4, marshalledProperties = org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, redeliveryCounter = 0, size = 0, properties = {IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer = false, text = <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486,\"eventId\":\"-1996324143\",\"relatedEventId\":null}}],\"harvested\":true,\"expiry\":1623930630497,\"errorUri\":null,\"harvestedDateTime\":1623325840029,\"flowInvocationEvents\":[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830491,\"endTimeMillis\":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id><asset><isin>XS2201078503</isin></asset><trade-details><nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7</amount><currency>USD</currency><trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book><party-details><counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491,\"eventId\":\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = null, producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out, transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = 1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, groupSequence = 0, targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util.ByteSequence@5e5e1ee4, marshalledProperties = org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, redeliveryCounter = 0, size = 0, properties = {IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer = false, text = <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486,\"eventId\":\"-1996324143\",\"relatedEventId\":null}}]}", (String)solrInputDocument.getFieldValue(SolrDaoBase.PAYLOAD_CONTENT), false);
+        JSONAssert.assertEquals("{\"id\":24271,\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\"," +
+            "\"invocationStartTime\":1623325830486,\"invocationEndTime\":1623325830496,\"finalAction\":\"PUBLISH\"," +
+            "\"componentInvocationMetricImpls\":[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\"," +
+            "\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null," +
+            "\"startTimeMillis\":1623325830491,\"endTimeMillis\":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\"" +
+            ":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id>" +
+            "<asset><isin>XS2201078503</isin></asset><trade-details><nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7<" +
+            "/amount><currency>USD</currency><trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book>" +
+            "<party-details><counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\"," +
+            "\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491," +
+            "\"eventId\":\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":" +
+            "\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier" +
+            "\":null,\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":" +
+            "{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:" +
+            "Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = " +
+            "null, producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out," +
+            " transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = " +
+            "1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, " +
+            "groupSequence = 0, targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util." +
+            "ByteSequence@5e5e1ee4, marshalledProperties = org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, " +
+            "redeliveryCounter = 0, size = 0, properties = {IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, " +
+            "readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer = false, text = <?xml version=\\\"1.0\\\" encoding=" +
+            "\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\",\"flowName\":\"Trade Consumer Flow\",\"componentName\":" +
+            "\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486,\"eventId\":\"-1996324143\",\"relatedEventId\":null}}]," +
+            "\"harvested\":true,\"expiry\":1623930630497,\"errorUri\":null,\"harvestedDateTime\":1623325840029,\"flowInvocationEvents\":" +
+            "[{\"componentName\":\"JMS Producer\",\"beforeEventIdentifier\":\"-1996324143\",\"beforeRelatedEventIdentifier\":null,\"" +
+            "afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null,\"startTimeMillis\":1623325830491,\"endTimeMillis\"" +
+            ":1623325830496,\"id\":561,\"metrics\":[],\"wiretapFlowEvent\":{\"identifier\":562,\"event\":\"<?xml version=\\\"1.0\\\" " +
+            "encoding=\\\"UTF-8\\\"?><bdm><trade><trade-id>88634252</trade-id><asset><isin>XS2201078503</isin></asset><trade-details>" +
+            "<nominal>895715</nominal><price>103.554</price><amount>7.7193923646E7</amount><currency>USD</currency>" +
+            "<trade-date-time>2021-06-10T12:50:30</trade-date-time></trade-details><book>GBP_GOVT</book><party-details>" +
+            "<counterparty>ACME Corporation</counterparty></party-details></trade></bdm>\",\"moduleName\":\"My Module\",\"flowName\":" +
+            "\"Trade Consumer Flow\",\"componentName\":\"JMS Producer\",\"timestamp\":1623325830491,\"expiry\":1623325830491,\"eventId\":" +
+            "\"-1996324143\",\"relatedEventId\":null}},{\"componentName\":\"JMS Consumer\",\"beforeEventIdentifier\":\"-1996324143\"," +
+            "\"beforeRelatedEventIdentifier\":null,\"afterEventIdentifier\":\"-1996324143\",\"afterRelatedEventIdentifier\":null," +
+            "\"startTimeMillis\":1623325830486,\"endTimeMillis\":1623325830487,\"id\":562,\"metrics\":[],\"wiretapFlowEvent\":" +
+            "{\"identifier\":561,\"event\":\"ActiveMQTextMessage {commandId = 5, responseRequired = false, messageId = ID:" +
+            "Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1:1, originalDestination = null, originalTransactionId = null, " +
+            "producerId = ID:Michaels-MacBook-Pro.local-55556-1623243068313-7:24490:1:1, destination = queue://com.caixa.bank.murex.out, " +
+            "transactionId = null, expiration = 0, timestamp = 1623325830485, arrival = 0, brokerInTime = 1623325830486, brokerOutTime = " +
+            "1623325830486, correlationId = null, replyTo = null, persistent = true, type = null, priority = 4, groupID = null, groupSequence = 0," +
+            " targetConsumerId = null, compressed = false, userID = null, content = org.apache.activemq.util.ByteSequence@5e5e1ee4, marshalledProperties = " +
+            "org.apache.activemq.util.ByteSequence@60f3cba8, dataStructure = null, redeliveryCounter = 0, size = 0, properties = " +
+            "{IkasanEventLifeIdentifier=-1996324143}, readOnlyProperties = true, readOnlyBody = true, droppable = false, jmsXGroupFirstForConsumer " +
+            "= false, text = <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\" standalo...rade></mxml>}\",\"moduleName\":\"My Module\"," +
+            "\"flowName\":\"Trade Consumer Flow\",\"componentName\":\"JMS Consumer\",\"timestamp\":1623325830486,\"expiry\":1623325830486," +
+            "\"eventId\":\"-1996324143\",\"relatedEventId\":null}}]}", (String)solrInputDocument.getFieldValue(SolrDaoBase.PAYLOAD_CONTENT)
+            , new CustomComparator(JSONCompareMode.LENIENT, new Customization("harvestedDateTime", (o1, o2) -> true)));
         Assert.assertEquals(1L, solrInputDocument.getFieldValue(SolrDaoBase.EXPIRY));
     }
 
