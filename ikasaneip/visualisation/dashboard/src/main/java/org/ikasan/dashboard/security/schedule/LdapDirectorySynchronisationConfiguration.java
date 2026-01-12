f7d1d6fffc7037ea5329dfd8e31889328b7af313
Michael Stewart <mick@humeenterprises.co.uk>
2026-01-12 07:48:39 +0000

    IKASAN-2672 adding system event logging and better logging to LDAP synchronisation.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/security/schedule/LdapDirectorySynchronisationConfiguration.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/security/schedule/LdapDirectorySynchronisationConfiguration.java
@@ -1,5 +1,6 @@
 package org.ikasan.dashboard.security.schedule;
 
+import org.ikasan.dashboard.ui.util.SystemEventLogger;
 import org.ikasan.scheduler.CachingScheduledJobFactory;
 import org.ikasan.scheduler.SchedulerFactory;
 import org.ikasan.security.service.LdapService;
@@ -13,8 +14,9 @@ public class LdapDirectorySynchronisationConfiguration {
 
     @Bean
     @DependsOn("dashboardSchedulerService")
-    public LdapDirectorySynchronisationSchedulerService ldapDirectorySynchronisationService(SecurityService securityService, LdapService ldapService) {
+    public LdapDirectorySynchronisationSchedulerService ldapDirectorySynchronisationService(SecurityService securityService
+        , LdapService ldapService, SystemEventLogger systemEventLogger) {
         return new LdapDirectorySynchronisationSchedulerService(SchedulerFactory.getInstance().getScheduler()
-            , CachingScheduledJobFactory.getInstance(), securityService, ldapService);
+            , CachingScheduledJobFactory.getInstance(), securityService, ldapService, systemEventLogger);
     }
 }
