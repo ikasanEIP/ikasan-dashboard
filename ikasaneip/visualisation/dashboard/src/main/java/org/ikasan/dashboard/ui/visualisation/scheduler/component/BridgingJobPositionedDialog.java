7296ddb1fc3c1f9f0a460c60072100b04849a972
Michael Stewart <mick@humeenterprises.co.uk>
2026-02-19 03:23:04 +0000

    IKASAN-2687 adding confirm dialog to interactions with bridging jobs.

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/component/BridgingJobPositionedDialog.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/scheduler/component/BridgingJobPositionedDialog.java
@@ -3,6 +3,8 @@ package org.ikasan.dashboard.ui.visualisation.scheduler.component;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.NativeLabel;
@@ -53,10 +55,20 @@ public class BridgingJobPositionedDialog extends PositionedDialog {
     private ContextInstance contextInstance;
 
 
+    /**
+     * Constructor for creating a BridgingJobPositionedDialog object.
+     *
+     * @param schedulerJobInstanceRecord The SchedulerJobInstanceRecord object associated with the dialog.
+     * @param logStreamingService The LogStreamingService used for log streaming.
+     * @param moduleMetaDataService The ModuleMetaDataService used for module metadata operations.
+     * @param schedulerJobInstanceService The SchedulerJobInstanceService for scheduler job instance operations.
+     * @param scheduledContextInstanceService The ScheduledContextInstanceService for scheduled context instance operations.
+     * @param contextInstance The ContextInstance object associated with the dialog.
+     */
     public BridgingJobPositionedDialog(SchedulerJobInstanceRecord schedulerJobInstanceRecord, LogStreamingService logStreamingService,
                                        ModuleMetaDataService moduleMetaDataService, SchedulerJobInstanceService schedulerJobInstanceService,
                                        ScheduledContextInstanceService scheduledContextInstanceService, ContextInstance contextInstance) {
-        super(100, 250);
+        super(90, 270);
         this.schedulerJobInstanceRecord = schedulerJobInstanceRecord;
         this.logStreamingService = logStreamingService;
         this.moduleMetaDataService = moduleMetaDataService;
@@ -66,34 +78,57 @@ public class BridgingJobPositionedDialog extends PositionedDialog {
         this.init();
     }
 
+    /**
+     * Initialize the dialog by setting up the button layout and adding necessary buttons based on the status of the
+     * scheduler job instance. If the status is WAITING, a button to execute the job is added with a confirmation dialog.
+     * If the status is COMPLETE, a button to reset the job is added.
+     */
     private void init() {
         HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.setMargin(false);
+
+        NativeLabel label = new NativeLabel(getTranslation("menu-item.bridging-job"));
+        buttonLayout.add(label);
+        buttonLayout.setVerticalComponentAlignment(FlexComponent.Alignment.CENTER, label);
 
         if(this.schedulerJobInstanceRecord.getSchedulerJobInstance().getStatus().equals(InstanceStatus.WAITING)) {
-            Icon runJobIcon = IconDecorator.decorate(new Icon(VaadinIcon.PLAY), getTranslation("tooltip.execute-bridging-job"
-                , UI.getCurrent().getLocale()), "14pt", IkasanColours.IKASAN_ORANGE);
-            runJobIcon.addClickListener((ComponentEventListener<ClickEvent<Icon>>) iconClickEvent -> {
-                if(ContextMachineCache.instance().containsInstanceIdentifier(this.contextInstance.getId())) {
-                    try {
-                        ContextMachine contextMachine = ContextMachineCache.instance().getByContextInstanceId(this.contextInstance.getId());
-                        contextMachine.broadcastLocalEvent(this.createSchedulerJobInitiationEvent((BridgingJobInstance) this.schedulerJobInstanceRecord.getSchedulerJobInstance()
-                            , contextMachine.getContext()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        NotificationHelper.showErrorNotification(getTranslation("error.execute-bridging-job"));
+            Button executeJobButton = new Button(getTranslation("button.execute"), VaadinIcon.PLAY.create());
+            executeJobButton.setIconAfterText(true);
+            executeJobButton.getElement().setAttribute("title", getTranslation("tooltip.execute-bridging-job"
+                , UI.getCurrent().getLocale()));
+            executeJobButton.addClickListener( event -> {
+                ConfirmDialog confirmDialog = new ConfirmDialog();
+                confirmDialog.setHeader(getTranslation("confirm-dialog.execute-bridging-job-header", UI.getCurrent().getLocale()));
+                confirmDialog.setText(getTranslation("confirm-dialog.execute-bridging-job-text", UI.getCurrent().getLocale()));
+                confirmDialog.setConfirmText(getTranslation("button.ok"));
+                confirmDialog.setCancelText(getTranslation("button.cancel"));
+                confirmDialog.setCancelable(true);
+                confirmDialog.open();
+
+                confirmDialog.addConfirmListener(confirmEvent -> {
+                    if (ContextMachineCache.instance().containsInstanceIdentifier(this.contextInstance.getId())) {
+                        try {
+                            ContextMachine contextMachine = ContextMachineCache.instance().getByContextInstanceId(this.contextInstance.getId());
+                            contextMachine.broadcastLocalEvent(this.createSchedulerJobInitiationEvent((BridgingJobInstance) this.schedulerJobInstanceRecord.getSchedulerJobInstance()
+                                , contextMachine.getContext()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            NotificationHelper.showErrorNotification(getTranslation("error.execute-bridging-job"));
+                        } finally {
+                            this.close();
+                            NotificationHelper.showErrorNotification(getTranslation("message.execute-bridging-job"));
+                        }
                     }
-                    finally {
-                        this.close();
-                        NotificationHelper.showErrorNotification(getTranslation("message.execute-bridging-job"));
-                    }
-                }
+                });
             });
-            buttonLayout.add(runJobIcon);
+            buttonLayout.add(executeJobButton);
         }
         else if(this.schedulerJobInstanceRecord.getSchedulerJobInstance().getStatus().equals(InstanceStatus.COMPLETE)) {
-            Icon resetJobIcon = IconDecorator.decorate(new Icon(VaadinIcon.ARROW_BACKWARD), getTranslation("tooltip.reset-bridging-job"
-                , UI.getCurrent().getLocale()), "14pt", IkasanColours.IKASAN_ORANGE);
-            resetJobIcon.addClickListener((ComponentEventListener<ClickEvent<Icon>>) iconClickEvent -> {
+            Button resetJobButton = new Button(getTranslation("button.reset"), VaadinIcon.ARROW_BACKWARD.create());
+            resetJobButton.setIconAfterText(true);
+            resetJobButton.getElement().setAttribute("title", getTranslation("tooltip.reset-bridging-job"
+                , UI.getCurrent().getLocale()));
+            resetJobButton.addClickListener(iconClickEvent -> {
                 if(ContextMachineCache.instance().containsInstanceIdentifier(this.contextInstance.getId())) {
                     try {
                         ContextMachine contextMachine = ContextMachineCache.instance().getByContextInstanceId(this.contextInstance.getId());
@@ -110,19 +145,20 @@ public class BridgingJobPositionedDialog extends PositionedDialog {
                     }
                 }
             });
-            buttonLayout.add(resetJobIcon);
+            buttonLayout.add(resetJobButton);
         }
 
-
-        VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.setWidth("100%");
-        NativeLabel label = new NativeLabel(getTranslation("menu-item.bridging-job"));
-        buttonLayout.getStyle().set("position", "absolute");
-        buttonLayout.getStyle().set("right", "20px");
-        verticalLayout.add(label, buttonLayout);
-        super.add(verticalLayout);
+        super.add(buttonLayout);
     }
 
+    /**
+     * Creates a SchedulerJobInitiationEvent based on the provided BridgingJobInstance and ContextInstance.
+     *
+     * @param bridgingJobInstance The BridgingJobInstance object to get agent name, job name, and child context names from.
+     * @param contextInstance The ContextInstance object to get name and ID from.
+     * @return The created SchedulerJobInitiationEvent with the agent name, job name, context name, context instance ID,
+     * child context names, and other necessary fields set.
+     */
     private SchedulerJobInitiationEvent createSchedulerJobInitiationEvent(BridgingJobInstance bridgingJobInstance
         , ContextInstance contextInstance) {
         SchedulerJobInitiationEvent schedulerJobInitiationEvent = new SchedulerJobInitiationEventImpl();
