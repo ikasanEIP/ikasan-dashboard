622cbdf960452d65f52e1b6b4687d0c7189c6924
Michael Stewart <mick@humeenterprises.co.uk>
2026-03-16 13:23:38 +0000

    IKASAN-2699 fixing issue where skipped job incorrectly assessed in part of logical grouping where preceding jobs had not completed.

--- ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/core/machine/AbstractLogicMachine.java
+++ ikasaneip/job-orchestration/core/src/main/java/org/ikasan/job/orchestration/core/machine/AbstractLogicMachine.java
@@ -35,7 +35,6 @@ public class AbstractLogicMachine<STATEFUL_ENTITY extends StatefulEntity> {
                     }
 
                     if (!statefulEntity.getStatus().equals(InstanceStatus.COMPLETE) &&
-                        !statefulEntity.getStatus().equals(InstanceStatus.SKIPPED) &&
                         !statefulEntity.getStatus().equals(InstanceStatus.SKIPPED_COMPLETE) &&
                         !(statefulEntity.getStatus().equals(InstanceStatus.ERROR)
                             && statefulEntity.isErrorAcknowledged() != null
