2fd2073bc10acf4ba9f0917263bed14694e900f5
Michael Stewart <mick@humeenterprises.co.uk>
2026-08-12 15:47:13 +0100

    IKASAN-2772 ongoing mongo implementation - development commit

---
+++ ikasaneip/persistence/mongo/src/main/java/org/ikasan/mongo/persistence/business/stream/metadata/model/Destination.java
@@ -0,0 +1,44 @@
+package org.ikasan.mongo.persistence.business.stream.metadata.model;
+
+/**
+ * Represents a destination in a business stream.
+ * Destinations are endpoints where data is sent.
+ */
+public class Destination {
+    private String id;
+    private String name;
+    private Integer x;
+    private Integer y;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
