b05db611b85b572faa269b0cdab560cec8180ce0
Michael Stewart <mick@humeenterprises.co.uk>
2026-08-20 10:50:37 +0100

    IKASAN-2775 removing commented out code

--- ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/cache/FlowStateCache.java
+++ ikasaneip/visualisation/dashboard/src/main/java/org/ikasan/dashboard/cache/FlowStateCache.java
@@ -167,8 +167,7 @@ public class FlowStateCache implements Consumer<FlowState>
              previousState.getState() == State.STOPPED_STATE ||
              previousState.getState() == State.RUNNING_STATE) &&
             previousState.getState() != currentState &&
-            isOscillationState;// &&
-            //timeSinceLastState <= oscillationWindowMs;
+            isOscillationState;
 
         // Update last state and time for future oscillation detection
         lastState.put(key, flowState);
